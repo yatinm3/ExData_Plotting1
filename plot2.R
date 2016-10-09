f973979cac77281e28a56102a86596c5b0c7d564
Yatin Majmudar <yatin.majmudar@gmail.com>
2016-10-08 20:25:31 -0700

    plot 2 done

---
+++ plot2.R
@@ -0,0 +1,28 @@
+#plot2.R:
+#This R-script creates a line plot of Global Active Power v/s weekdays.....over a period of 
+#two days: 2007-02-01 2007-02-02
+#
+#
+# Download file and unzip in local directory
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+fnameZip <- "household_power_consumption.zip"
+download.file(fileUrl, destfile=fnameZip, method="curl")
+unzip(fileZip)
+
+fnameMeter <- "household_power_consumption.txt"
+dfInput <- read.table(fnameMeter, header=TRUE, sep=";", na.strings="?")
+
+#Add a column posixDate based on Date/Time columns
+#dfDate <- strptime(paste(df$Date,df$Time), "%d/%m/%Y %H:%M:%S")
+dfInput <- data.frame(dfInput, posixDate = strptime(paste(dfInput$Date,dfInput$Time), "%d/%m/%Y %H:%M:%S"))
+
+#Subset data between Feb01 and Feb02 2007
+date2007Feb01 <- strptime(x="01/02/2007 00:00:00",format="%d/%m/%Y %H:%M:%S" )
+date2007Feb03 <- strptime(x="03/02/2007 00:00:00",format="%d/%m/%Y %H:%M:%S" )
+df <- subset(dfInput, (posixDate >= date2007Feb01) & (posixDate < date2007Feb03))
+
+# line plot on-screen and make sure it looks ok
+plot(df$posixDate, df$Global_active_power, type= "l", ylab= "Global Active Power (kilowatts)", xlab="")
+# copy to device
+dev.copy(png,"plot2.png")
+dev.off()
